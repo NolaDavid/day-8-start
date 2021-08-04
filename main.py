15c3f4d46bbc1946448a4433618599a532d10fa8
NolaDavid <dguillory62888@gmail.com>
2021-08-04 00:38:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+def greet():
+    print('This worked')
+    print('This worked')
+    print('This worked')
+
+greet()
+#function that allows for input 
+def greet_with_name(name):
+  print(f'Hello  {name}')
+  print(f'How do you do {name}')
+  print(f' Isnt the weather nice today?')
+
+greet_with_name('David')
