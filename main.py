4cba6c767b73f582aab1358179454d13d849fce9
NolaDavid <dguillory62888@gmail.com>
2021-08-04 00:44:11 +0000

    working with functions with multiple inputs

--- main.py
+++ main.py
@@ -2,16 +2,22 @@
 # Create a function called greet(). 
 # Write 3 print statements inside the function.
 # Call the greet() function and run your code.
-def greet():
-    print('This worked')
-    print('This worked')
-    print('This worked')
+# def greet():
+#     print('This worked')
+#     print('This worked')
+#     print('This worked')
 
-greet()
-#function that allows for input 
-def greet_with_name(name):
-  print(f'Hello  {name}')
-  print(f'How do you do {name}')
-  print(f' Isnt the weather nice today?')
+# greet()
+# #function that allows for input 
+# def greet_with_name(name):
+#   print(f'Hello  {name}')
+#   print(f'How do you do {name}')
+#   print(f' Isnt the weather nice today?')
 
-greet_with_name('David')
+# greet_with_name('David')
+
+
+def greet_with(name, location):
+  print(f'Hello  my name is {name}, and im from {location}')
+
+greet_with('David', 'Phoenix')
